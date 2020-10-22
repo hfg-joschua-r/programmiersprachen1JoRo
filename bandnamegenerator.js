60af435063eda51ced351c5fa32bbcec60b70cb4
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2020-10-22 16:04:05 +0200

    übung2 added

--- bandnamegenerator.js
+++ bandnamegenerator.js
@@ -1,9 +1,28 @@
 //array mit adjektiven
 //array mit substantiven
-let adjektive = ["incredible ", "astonishing ", "super duper ", "sexy ", "depressed ", "frustrated ", "extreme ", "homeless ", "clumsy "];
+let adjektive = ["incredible", "astonishing", "super duper", "sexy", "depressed", "frustrated", "extreme", "homeless", "clumsy"];
 let substantive = ["Tigers", "Pandas", "mirrors", "feathers", "programmers", "policewomen/men"]
 //function: combine random adjektiv mit random substantiv
+let randomBandname = (adj, sub) => console.log("The " + adj + " " + sub);
 
-let randomBandname = (adj, sub) => console.log("The " + adj + sub);  
+randomBandname(adjektive[Math.floor(Math.random() * adjektive.length)], substantive[Math.floor(Math.random() * substantive.length)]);
+//Teil 2: angleichen von adjektiven
+let artikel = ["der", "die", "das"];
+let deAdjektive = ["bester", "schönster", "fleißigster"]
 
-randomBandname(adjektive[Math.floor(Math.random() * adjektive.length)], substantive[Math.floor(Math.random()* substantive.length)]);
+let art = artikel[Math.floor(Math.random() * artikel.length)];
+let deAdj = deAdjektive[Math.floor(Math.random() * deAdjektive.length)];
+
+//switch case für der, die, das
+switch (art) {
+    case "der":
+        console.log(art + " " + deAdj.substr(0, deAdj.length - 1));
+        break;
+    case "die":
+        deAdj = deAdj.substr(0, deAdj.length - 1);
+        console.log(art + " " + deAdj.concat("n"));
+        break;
+    case "das":
+        console.log(art + " " + deAdj.substr(0, deAdj.length - 1));
+        break;
+}
