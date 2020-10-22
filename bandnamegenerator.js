18b64a40712f720676dee77452addfc5e06518e5
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2020-10-22 17:15:00 +0200

    Added Substantive to Übung2

--- bandnamegenerator.js
+++ bandnamegenerator.js
@@ -8,8 +8,11 @@ let randomBandname = (adj, sub) => console.log("The " + adj + " " + sub);
 randomBandname(adjektive[Math.floor(Math.random() * adjektive.length)], substantive[Math.floor(Math.random() * substantive.length)]);
 
 //Teil 2: angleichen von adjektiven an artikel 
-let artikel = ["der", "die", "das"];
+let artikel = ["der", "die", "das"]
 let deAdjektive = ["bester", "schönster", "fleißigster"]
+let mSingularSubstantive = ["Junge", "Herd", "Kapitän", "Vikinger"]
+let fPlurarSubtantive = ["Prinzessinen", "Arbeiten", "Aufgaben", "Vollzugsanstalten"]
+let dasSingualrSubstantive = ["Werk", "Baby", "Nutella", "Bananenbrot"]
 
 let art = artikel[Math.floor(Math.random() * artikel.length)];
 let deAdj = deAdjektive[Math.floor(Math.random() * deAdjektive.length)];
@@ -17,13 +20,13 @@ let deAdj = deAdjektive[Math.floor(Math.random() * deAdjektive.length)];
 //switch case für der, die, das
 switch (art) {
     case "der":
-        console.log(art + " " + deAdj.substr(0, deAdj.length - 1));
+        console.log(art + " " + deAdj.substr(0, deAdj.length - 1) + " " + mSingularSubstantive[Math.floor(Math.random() * mSingularSubstantive.length)]);
         break;
     case "die":
         deAdj = deAdj.substr(0, deAdj.length - 1);
-        console.log(art + " " + deAdj.concat("n"));
+        console.log(art + " " + deAdj.concat("n") + " " + fPlurarSubtantive[Math.floor(Math.random() * fPlurarSubtantive.length)]);
         break;
     case "das":
-        console.log(art + " " + deAdj.substr(0, deAdj.length - 1));
+        console.log(art + " " + deAdj.substr(0, deAdj.length - 1) + " " + dasSingualrSubstantive[Math.floor(Math.random() * dasSingualrSubstantive.length)]);
         break;
 }
