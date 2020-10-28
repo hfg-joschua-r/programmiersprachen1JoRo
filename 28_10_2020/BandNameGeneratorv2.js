ed6276f728acc68b0d54c3455ea7451bf7a63992
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2020-10-28 12:08:57 +0100

    Update BandNameGeneratorv2.js

--- 28_10_2020/BandNameGeneratorv2.js
+++ 28_10_2020/BandNameGeneratorv2.js
@@ -4,7 +4,11 @@ const Das = { article: "Das", plural: false }
 const DiePl = { article: "Die", plural: true }
 
 let artikel = [Der, Die, Das, DiePl]
-let adjektive = ["groß", "wunderbar", "einzigartig", "unverständlich"]
+
+let adjektive = [["groß", "größer", "größte"], ["gut", "besser", "beste"], ["schön", "schöner", "schönste"]]
+let TESTadjektive = ["groß", "wunderbar", "einzigartig", "unverständlich"]
+
+
 let mSubstantive = ["Performer", "Reiter", "Knecht"]
 let fSubstantive = ["Marmelade", "Kuh", "Schnecke"]
 let dSubstantive = ["Quartett", "Gewitter", "Abendprogramm"]
@@ -13,22 +17,46 @@ let pSubstantive = ["Musketiere", "Programmierer", "Verlierer"]
 for (let i = 0; i < 10; i++) {
     let art = artikel[Math.floor(Math.random() * artikel.length)];
     let adj = adjektive[Math.floor(Math.random() * adjektive.length)];
+    adj = adj[Math.floor(Math.random() * adj.length)];
 
-    if (!art.plural) {
-        //singular
-        switch(art.article){
-            case "Der":
-                console.log(art.article + " " + adj.concat("e") + " " + mSubstantive[Math.floor(Math.random() * mSubstantive.length)])
-                break;
-            case "Die":
-                console.log(art.article + " " + adj.concat("e") + " " + fSubstantive[Math.floor(Math.random() * fSubstantive.length)])
-                break;
-            case "Das":
-                console.log(art.article + " " + adj.concat("e") + " " + dSubstantive[Math.floor(Math.random() * dSubstantive.length)])
-                break;
+    if(adj.slice(-1) === "e"){
+        if (!art.plural) {
+            //singular
+            switch(art.article){
+                case "Der":
+                    console.log(art.article + " " + adj + " " + mSubstantive[Math.floor(Math.random() * mSubstantive.length)])
+                    break;
+                case "Die":
+                    console.log(art.article + " " + adj + " " + fSubstantive[Math.floor(Math.random() * fSubstantive.length)])
+                    break;
+                case "Das":
+                    console.log(art.article + " " + adj + " " + dSubstantive[Math.floor(Math.random() * dSubstantive.length)])
+                    break;
+            }
+        }
+        else {
+            console.log(art.article + " " + adj + " " + pSubstantive[Math.floor(Math.random() * pSubstantive.length)])
         }
     }
+
     else {
-        console.log(art.article + " " + adj.concat("en") + " " +  pSubstantive[Math.floor(Math.random() * pSubstantive.length)])
+        if (!art.plural) {
+            //singular
+            switch(art.article){
+                case "Der":
+                    console.log(art.article + " " + adj.concat("e") + " " + mSubstantive[Math.floor(Math.random() * mSubstantive.length)])
+                    break;
+                case "Die":
+                    console.log(art.article + " " + adj.concat("e") + " " + fSubstantive[Math.floor(Math.random() * fSubstantive.length)])
+                    break;
+                case "Das":
+                    console.log(art.article + " " + adj.concat("e") + " " + dSubstantive[Math.floor(Math.random() * dSubstantive.length)])
+                    break;
+            }
+        }
+        else {
+            console.log(art.article + " " + adj.concat("en") + " " + pSubstantive[Math.floor(Math.random() * pSubstantive.length)])
+        }
+
     }
 }
