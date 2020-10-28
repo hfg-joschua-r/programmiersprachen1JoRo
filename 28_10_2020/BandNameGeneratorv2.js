da9fba4f44f0222a3adb05f698031687551d468d
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2020-10-28 12:16:10 +0100

    added comments

--- 28_10_2020/BandNameGeneratorv2.js
+++ 28_10_2020/BandNameGeneratorv2.js
@@ -2,27 +2,29 @@ const Der = { article: "Der", plural: false }
 const Die = { article: "Die", plural: false }
 const Das = { article: "Das", plural: false }
 const DiePl = { article: "Die", plural: true }
-
+//Array mit artikel Objekten
 let artikel = [Der, Die, Das, DiePl]
 
+//adjektive mit verschieden Steigerungen
 let adjektive = [["groß", "größer", "größte"], ["gut", "besser", "beste"], ["schön", "schöner", "schönste"]]
-let TESTadjektive = ["groß", "wunderbar", "einzigartig", "unverständlich"]
-
 
 let mSubstantive = ["Performer", "Reiter", "Knecht"]
 let fSubstantive = ["Marmelade", "Kuh", "Schnecke"]
 let dSubstantive = ["Quartett", "Gewitter", "Abendprogramm"]
 let pSubstantive = ["Musketiere", "Programmierer", "Verlierer"]
 
+//soll 10 mal ausgegeben werden 
 for (let i = 0; i < 10; i++) {
+    //Wählen von zufälligen Artikeln und Adjektiven mit Steigungen
     let art = artikel[Math.floor(Math.random() * artikel.length)];
     let adj = adjektive[Math.floor(Math.random() * adjektive.length)];
     adj = adj[Math.floor(Math.random() * adj.length)];
 
-    if(adj.slice(-1) === "e"){
+    //Überprüfung ob Adjektiv auf e endet ---> Wenn Ja? Dann werden zusätzliche Endungen obsolet
+    if (adj.slice(-1) === "e") {
         if (!art.plural) {
-            //singular
-            switch(art.article){
+            //Fall singular
+            switch (art.article) {
                 case "Der":
                     console.log(art.article + " " + adj + " " + mSubstantive[Math.floor(Math.random() * mSubstantive.length)])
                     break;
@@ -35,14 +37,15 @@ for (let i = 0; i < 10; i++) {
             }
         }
         else {
+            //Fall Plural
             console.log(art.article + " " + adj + " " + pSubstantive[Math.floor(Math.random() * pSubstantive.length)])
         }
     }
 
     else {
         if (!art.plural) {
-            //singular
-            switch(art.article){
+            //Fall singular
+            switch (art.article) {
                 case "Der":
                     console.log(art.article + " " + adj.concat("e") + " " + mSubstantive[Math.floor(Math.random() * mSubstantive.length)])
                     break;
@@ -55,6 +58,7 @@ for (let i = 0; i < 10; i++) {
             }
         }
         else {
+            //Fall plural
             console.log(art.article + " " + adj.concat("en") + " " + pSubstantive[Math.floor(Math.random() * pSubstantive.length)])
         }
 
