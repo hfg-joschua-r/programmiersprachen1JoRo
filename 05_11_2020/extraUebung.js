170cb20df15af234ba1c3280eb74ab5e9f671144
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2020-11-05 14:55:27 +0100

    updated extraUebung, added comments

--- 05_11_2020/extraUebung.js
+++ 05_11_2020/extraUebung.js
@@ -2,16 +2,25 @@
 "Lege ein Array mit 10 verschiedenen Wörtern an, die nacheinander zufällig ausgewählt und ausgegeben werden sollen.
 Jedes Wort darf nur einmal ausgegeben werden." */
 
+//array mit random wörtern
 const words = ["Hase", "Orangensaft", "Brille", "Lampe", "Tiger", "Schreibtisch", "Schubladen", "Kabel", "Motor", "Rasen"]
+//leeres array in der später bereits verwendete Wörter gespeichert werden
 let usedWords = []
 
-//choose random word
+//10 wörter random ausgeben
 for(let i = 1; i <= words.length; i ++){
-
+    //Hier wird ein random Wort ausgewählt
     let chosenWord = words[Math.floor(Math.random() * words.length)]
-    if(usedWords.includes(chosenWord)){ continue }
+    //wenn das Wort bereits verwendet wurde (es sich im usedWords array befindet):
+    if(usedWords.includes(chosenWord)){
+        //Da wir ein neues Wort brauchen ziehen wir von i eins ab und beenden mit continue diesen "Durchlauf"
+         i--;
+         continue
+     }
     else{
+        //Wenn das wort noch nicht in unserem Array existiert gib es in der Konsole aus. 
         console.log("Das " + i + "te Wort ist " + chosenWord)
+        //Und füge es zu unserem usedWords array hinzu
         usedWords.push(chosenWord);
     }
 }
