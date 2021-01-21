a9727089f713fb809815e4f3f93856fa48157d58
joschroth <joschua.rothenbacher@hfg.design>
2021-01-21 14:04:46 +0100

    Updated File Directories and added AI functions

--- ticTacToe/ticScript.js
+++ ticTacToe/ticScript.js
@@ -1,6 +1,7 @@
 //Create our variables
 let currentColor = "";
 let gameRunning = false;
+let inAiMode = false;
 let teamToMove = "";
 let pointsToWin;
 let gameSize; //We need this variable later on when it comes to check for wins
@@ -11,12 +12,15 @@ let pointsR = 0;
 //store our player names
 let playerNameOne;
 let playerNameTwo;
+//add the symbols for the player 
+let playerOne = 'b';
+let playerTwo = 'o';
 //adding our some soundeffects here
-let bumm = new Audio("bumm.wav");
-let chack = new Audio("chack.flac");
-let beep1 = new Audio("beep1.wav");
-let beep2 = new Audio("beep2.wav");
-let victory = new Audio("victory.mp3");
+let bumm = new Audio("./resources/audioFiles/bumm.wav"); 
+let chack = new Audio("./resources/audioFiles/chack.flac");
+let beep1 = new Audio("./resources/audioFiles/beep1.wav");
+let beep2 = new Audio("./resources/audioFiles/beep2.wav");
+let victory = new Audio("./resources/audioFiles/victory.mp3");
 
 //We want to adjust our x or y value for our gamefield so that its only possible to create squares + adjust the player names to the left and right.
 function onGameSizeValueChange() {
@@ -47,6 +51,7 @@ function settingsMenu() {
   if (ptw > gX) {
     //bug: wenn 10x10 spielfeld kommen wir hier rein, unabhängig welchge ptw man wählt???
     console.log(ptw + " > " + gX);
+    //
     alert("Points to win have to be less than the game Size!");
   } else {
     //currently we only support game fields up to a size of 10x10
@@ -94,7 +99,9 @@ function initUI(gameSizeY, p) {
   //set the value of how many equal symbols in a line are needed to win a game
   pointsToWin = p;
   addEventListeners();
+  if(!inAiMode) {
   determineStart();
+  }
 }
 
 //Add event listeners to grids! For clicks and mouseOver effects/events
@@ -127,13 +134,13 @@ function determineStart() {
       $("#header").css("color", "#3F88C5");
       console.log("test!");
       currentColor = "#3F88C5";
-      teamToMove = "b";
+      teamToMove = playerOne;
     } else {
       //player Two begins
       $("#header").html(playerNameTwo + " begins!");
       $("#header").css("color", "#e76f51");
       currentColor = "#e76f51";
-      teamToMove = "o";
+      teamToMove = playerTwo;
     }
   } else {
     //bug: sometimes this is called twice, due to unknown reasons
@@ -143,24 +150,25 @@ function determineStart() {
 
 //this function is called everytime the user clicks on a field
 function handleGridClick(selGrid) {
-  console.log(currentColor);
   //we determine the position of the current Grid by identifying it's id
   let pos = selGrid.id;
   //if the grid is still empty we can fill in our Team
   if (selGrid.childNodes[0].innerHTML === "") {
     gameField[pos[0]][pos[1]] = teamToMove;
     //change team thats on the move, add content to our Field and change Colors
-    if (teamToMove === "o") {
+    if (teamToMove === playerTwo) {
       bumm.play();
       selGrid.childNodes[0].innerHTML = "O";
       currentColor = "#3F88C5";
-      teamToMove = "b";
+      teamToMove = playerOne;
       $("#header").html(playerNameOne + ", it's your turn!");
       $("#header").css("color", currentColor);
-    } else if (teamToMove === "b") {
+    }
+    
+    else if (teamToMove === playerOne) {
       chack.play();
       selGrid.childNodes[0].innerHTML = "X";
-      teamToMove = "o";
+      teamToMove = playerTwo;
       currentColor = "#e76f51";
       $("#header").html(playerNameTwo + ", it's your turn!");
       $("#header").css("color", currentColor);
@@ -168,7 +176,6 @@ function handleGridClick(selGrid) {
   } else {
     $("#header").html("Please select an empty field!");
   }
-
   //call our function if we have a win situation
   checkForWin();
 }
@@ -180,9 +187,11 @@ function checkForWin() {
   let curLineV = "";
   let curLineH = "";
 
+  let winner = null;
+
   //set our Win condition by repeating the teams letters the number of times
-  let winCondition1 = "b".repeat(pointsToWin);
-  let winCondition2 = "o".repeat(pointsToWin);
+  let winCondition1 = playerOne.repeat(pointsToWin);
+  let winCondition2 = playerTwo.repeat(pointsToWin);
   //checking vertical and horizontal wins
   for (let i = 0; i < gameSize; i++) {
     curLineV = "";
@@ -194,11 +203,23 @@ function checkForWin() {
       curLineH += gameField[j][i];
     }
     //check our generated strings against our winconditions
-    if (curLineV.includes(winCondition1)) win("b", i);
-    else if (curLineV.includes(winCondition2)) win("o", i);
+    if (curLineV.includes(winCondition1)) {
+      win(playerOne, i); 
+      return playerOne;
+    }
+    else if (curLineV.includes(winCondition2)) {
+      win(playerOne, i); 
+      return playerTwo;
+    }
 
-    if (curLineH.includes(winCondition1)) win("b", i);
-    else if (curLineH.includes(winCondition2)) win("o", i);
+    if (curLineH.includes(winCondition1)){
+      win(playerOne, i); 
+      return playerOne;
+    }
+    else if (curLineH.includes(winCondition2)){
+      win(playerOne, i); 
+      return playerTwo;
+    }
   }
 
   //check diagonals
@@ -221,8 +242,14 @@ function checkForWin() {
           }
 
           //check for win
-          if (curLineD.includes(winCondition1)) win("b", i);
-          else if (curLineD.includes(winCondition2)) win("o", i);
+          if (curLineD.includes(winCondition1)) {
+            win(playerOne, i); 
+            return playerOne;
+          }
+          else if (curLineD.includes(winCondition2)){
+            win(playerOne, i); 
+            return playerTwo;
+          }
         }
       }
       if (j - (pointsToWin - 1) >= 0) {
@@ -230,8 +257,14 @@ function checkForWin() {
           for (let m = 0; m < pointsToWin; m++) {
             curLineD2 += gameField[i + m][j - m];
           }
-          if (curLineD2.includes(winCondition1)) win("b", i);
-          else if (curLineD2.includes(winCondition2)) win("o", i);
+          if (curLineD2.includes(winCondition1)) {
+            win(playerOne, i); 
+            return playerOne;
+          }
+          else if (curLineD2.includes(winCondition2)) {
+            win(playerOne, i); 
+            return playerTwo;
+          }
         }
       }
     }
@@ -251,6 +284,10 @@ function checkForWin() {
     $("div.outer-grid").append(
       '<button class="resetButton" style="bottom: 7%;" onclick="playAgain()">Play again!</button>'
     );
+    return 'tie';
+  }
+  else {
+    return winner;
   }
 }
 
@@ -336,3 +373,226 @@ function openSettings() {
 </div>`);
 }
 
+/* -----------------------------------------------------------------------
+  here starts the experimental AI Stuff! currenty WIP
+   -----------------------------------------------------------------------
+*/
+//We start by only looking at 3x3, later on maybe at variable game fields!
+
+//implemnt minimax 
+//tipp von Flo: hardcode first move
+//we need an array with possible moves
+//tree diagram == recursive algorythm. 
+//loop through all moves
+
+//we need a new function to switch players
+//replace o and b in CheckForWin with "teamname"!
+
+//bug: die ai spielt gegen sich selber durch und hängt sich dann auf
+
+function aiMode(){
+  //the AI Will always take orange/playerTwo
+  inAiMode = true;
+  initUI(3,3);
+  bestMove();
+
+}
+
+//this is the ai move
+function bestMove(){ 
+  console.log(gameField);
+  let bestScore = -Infinity;
+  let moveI;
+  let moveJ; 
+
+  for (let i = 0; i < 3; i++){
+    for (let j = 0; j < 3; j++){
+      if(gameField[i][j] == ''){
+        gameField[i][j] = playerTwo;
+        let score = minimax(gameField, 0, false);
+        gameField[i][j] = '';
+      
+        if(score > bestScore) {
+          bestScore = score;
+          moveI = i;
+          moveJ = j;
+         
+        }
+      }
+    }
+  }
+  gameField[moveI, moveJ] = playerTwo;
+  changePlayer(moveI, moveJ)
+}
+
+let scores = {
+    'b': 1,
+    'o':-1,
+    'tie':0
+}
+
+function minimax(board, depth, isMaximizing){
+  let result = checkForWinAI();
+  if(result !== null){
+      console.log(gameField)
+      return scores[result]
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (gameField[i][j] == '') {
+          gameField[i][j] = playerTwo;
+          let score = minimax(board, depth + 1, false);
+          gameField[i][j] = '';
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  } 
+  else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (gameField[i][j] == '') {
+          gameField[i][j] = playerOne;
+          let score = minimax(board, depth + 1, true);
+          gameField[i][j] = '';
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  }
+}
+
+function changePlayer(x, y) {
+  //if the grid is still empty we can fill in our Team
+    let chosenID = x + '' + y;
+    gameField[x][y] = teamToMove;
+    //change team thats on the move, add content to our Field and change Colors
+      bumm.play();
+      
+     $('#' + chosenID).childNodes[0].innerHTML = "O";
+      currentColor = "#3F88C5";
+      teamToMove = playerOne;
+      $("#header").html(playerNameOne + ", it's your turn!");
+      $("#header").css("color", currentColor);
+      //call our function if we have a win situation
+      //checkForWin();
+}
+
+//Look for possible Wins
+function checkForWinAI() {
+  //log our current state of the gameField
+  //we store each line in the grid in these variables
+  let curLineV = "";
+  let curLineH = "";
+
+  let winner = null;
+
+  //set our Win condition by repeating the teams letters the number of times
+  let winCondition1 = playerOne.repeat(pointsToWin);
+  let winCondition2 = playerTwo.repeat(pointsToWin);
+  //checking vertical and horizontal wins
+  for (let i = 0; i < gameSize; i++) {
+    curLineV = "";
+    curLineH = "";
+    //loop through each and every Field
+    for (let j = 0; j < gameField[i].length; j++) {
+      //add the content of the field to our string
+      curLineV += gameField[i][j];
+      curLineH += gameField[j][i];
+    }
+    //check our generated strings against our winconditions
+    if (curLineV.includes(winCondition1)) {
+     
+      return playerOne;
+    }
+    else if (curLineV.includes(winCondition2)) {
+      
+      return playerTwo;
+    }
+
+    if (curLineH.includes(winCondition1)){
+      
+      return playerOne;
+    }
+    else if (curLineH.includes(winCondition2)){
+       
+      return playerTwo;
+    }
+  }
+
+  //check diagonals
+  //diagonal line from the left top to the right bottom
+  let curLineD = "";
+  //diagonal line from the top right to the bottom left
+  let curLineD2 = "";
+  for (let i = 0; i < gameSize; i++) {
+    //gameField[i][0] is our horizontal field
+    //gameField[0][j] is our vertical field
+    for (let j = 0; j < gameSize; j++) {
+      //in here we check every single Gamefield from up left to down right
+      curLineD = "";
+      curLineD2 = "";
+      //check that we dont try to acess values of our array, that do not exist (values which are out of our current boundary)
+      if (i + (pointsToWin - 1) < gameSize) {
+        if (j + (pointsToWin - 1) < gameSize) {
+          for (let m = 0; m < pointsToWin; m++) {
+            curLineD += gameField[i + m][j + m];
+          }
+
+          //check for win
+          if (curLineD.includes(winCondition1)) {
+           
+            return playerOne;
+          }
+          else if (curLineD.includes(winCondition2)){
+           
+            return playerTwo;
+          }
+        }
+      }
+      if (j - (pointsToWin - 1) >= 0) {
+        if (i + (pointsToWin - 1) < gameSize) {
+          for (let m = 0; m < pointsToWin; m++) {
+            curLineD2 += gameField[i + m][j - m];
+          }
+          if (curLineD2.includes(winCondition1)) {
+          
+            return playerOne;
+          }
+          else if (curLineD2.includes(winCondition2)) {
+            
+            return playerTwo;
+          }
+        }
+      }
+    }
+  }
+  let isThereAnEmptyFieldLeft = false;
+  //check if our whole field is full, in which case we have a draw
+  for (let i = 0; i < gameSize; i++) {
+    for (let j = 0; j < gameSize; j++) {
+      if (gameField[i][j] == "") {
+        isThereAnEmptyFieldLeft = true;
+      }
+    }
+  }
+   //this is executed if we have a draw, we add our play Again button.
+   if (!isThereAnEmptyFieldLeft) {
+    $("#header").html("There is no winner! We have a draw.");
+    $("div.outer-grid").append(
+      '<button class="resetButton" style="bottom: 7%;" onclick="playAgain()">Play again!</button>'
+    );
+    return 'tie';
+  }
+  else {
+    return winner;
+  }
+}
